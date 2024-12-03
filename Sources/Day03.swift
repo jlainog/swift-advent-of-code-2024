24e1b9e8330fb2462851a3dbfa3b3560bf76592e
Jaime Andres Laino Guerra <jlainog@gmail.com>
2024-12-03 11:02:03 -0500

    day 3

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -7,16 +7,106 @@ struct Day03: AdventDay {
   var data: String
 
   func parseInput() throws -> [Int] {
-    return [Int]()
+    let mulParser = Parse {
+      "mul("
+      Int.parser()
+      ","
+      Int.parser()
+      ")"
+    }.map { $0.0 * $0.1 }
+    let mulOptionsParser = OneOf {
+      Parse {
+        Skip {
+          PrefixUpTo("mul(")
+        }
+        mulParser
+      }
+
+      Parse {
+        Skip {
+          PrefixUpTo("mul(")
+        }
+        "mul("
+        Int.parser()
+        ","
+        Int.parser()
+      }.map { _ in 0 }
+
+      Parse {
+        Skip {
+          PrefixUpTo("mul(")
+        }
+        "mul("
+        Int.parser()
+        ","
+      }.map { _ in 0 }
+
+      Parse {
+        Skip {
+          PrefixUpTo("mul(")
+        }
+        "mul("
+        Int.parser()
+      }.map { _ in 0 }
+
+      Parse {
+        Skip {
+          PrefixUpTo("mul(")
+        }
+        "mul("
+      }.map { _ in 0 }
+    }
+
+    let parser = Many {
+      mulOptionsParser
+    } separator: {
+      PrefixUpTo("mul(")
+    } terminator: {
+      Rest()
+    }
+
+    return try parser.parse(data)
   }
 
   func part1() async throws -> Int {
     let input = try parseInput()
-    throw PartUnimplemented(day: day, part: 1)
+    return input.reduce(0, +)
   }
 
   func part2() async throws -> Int {
-    let input = try parseInput()
-    throw PartUnimplemented(day: day, part: 2)
+    enum Operation {
+      case `do`
+      case dont
+      case mul(Int)
+      case noop
+    }
+
+    let mulParser = Parse {
+      "mul("
+      Int.parser()
+      ","
+      Int.parser()
+      ")"
+    }.map { $0.0 * $0.1 }
+    let parser = Many {
+      OneOf {
+        "do()".map { Operation.do }
+        "don't()".map { Operation.dont }
+        mulParser.map { Operation.mul($0) }
+        Skip { First() }.map { Operation.noop }
+      }
+    }
+    let input = try parser.parse(data)
+    var doOp = true
+    return input.reduce(0) {
+      switch $1 {
+      case .do: doOp = true
+      case .dont: doOp = false
+      case .noop: break
+      case .mul(let valule):
+        return doOp ? $0 + valule : $0
+      }
+      return $0
+    }
   }
 }
